c7bcb4a70a7c11bc60f178dbd55a1403da9c2dc8
James Zimmer <abstractdeveloperx@gmail.com>
2026-03-01 17:14:18 -0700

    v1.0.1 update

--- Core/Systems/MusicBackportingSystem.cs
+++ Core/Systems/MusicBackportingSystem.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using BoulderBackport.Common.ID;
+using BoulderBackport.Content.Configs;
 using Terraria;
 using Terraria.Audio;
 using Terraria.ModLoader;
@@ -13,13 +14,29 @@ public class MusicBackportingSystem : BackportingSystemBase
     public override void Load()
     {
         if (Main.audioSystem is not LegacyAudioSystem legacyAudioSystem ||
-            legacyAudioSystem.AudioTracks.Length >= NewMusicID.Count) return;
+            legacyAudioSystem.AudioTracks.Length >= NewMusicID.Count || !AnyMusicConfigEnabled())
+            return;
 
         Array.Resize(ref legacyAudioSystem.AudioTracks, NewMusicID.Count);
+
         for (var i = NewMusicID.Destroyer; i < NewMusicID.Count; i++)
         {
             BoulderBackport.Instance.DebugLog($"Loading music ID {i}");
             legacyAudioSystem.LoadCue(i, $"Music_{i}");
         }
+    }
+
+    private static bool AnyMusicConfigEnabled()
+    {
+        var config = BackportConfig.Instance;
+        return config.KingSlimeBossMusic
+               || config.EaterOfWorldsBossMusic
+               || config.QueenBeeBossMusic
+               || config.SkeletronBossMusic
+               || config.TheTwinsBossMusic
+               || config.TheDestroyerBossMusic
+               || config.SkeletronPrimeBossMusic
+               || config.LunaticCultistBossMusic
+               || config.TorchGodBossMusic;
     }
 }
